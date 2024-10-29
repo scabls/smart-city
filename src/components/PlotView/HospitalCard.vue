25bdf859e0754bdc46ab1d5d117511cf0c68cfbd
scabls <caocy2024+gitlocal00@gmail.com>
2024-10-29 19:10:14 +0800

    02-07:第四第五个卡片

--- src/components/PlotView/HospitalCard.vue
+++ src/components/PlotView/HospitalCard.vue
@@ -1,9 +1,57 @@
 <template>
-  <CommonCard title="武汉三甲医院"> </CommonCard>
+  <CommonCard title="武汉三甲医院">
+    <div class="list">
+      <div v-for="item in data" :key="item.title" class="item">
+        <p>{{ item.title + item.value + item.quantifier }}</p>
+        <img :src="item.src" :width="item.size" :height="item.size" />
+      </div>
+    </div>
+  </CommonCard>
 </template>
 
 <script setup>
 import CommonCard from './CommonCard.vue'
+import { ref } from 'vue'
+
+const data = ref([
+  {
+    title: '医院',
+    value: '30',
+    quantifier: '家',
+    src: './icons/医院.png',
+    size: 65,
+  },
+  {
+    title: '门诊部',
+    value: '300',
+    quantifier: '个',
+    src: './icons/门诊.png',
+    size: 40,
+  },
+  {
+    title: '病床',
+    value: '3000',
+    quantifier: '张',
+    src: './icons/病床.png',
+    size: 50,
+  },
+])
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.list {
+  display: flex;
+  padding-top: 1.3rem;
+  font-size: 0.8rem;
+  .item {
+    flex: 1;
+    display: grid;
+    justify-items: center;
+    // align-content: center;
+    gap: 1rem;
+    img {
+      display: block;
+    }
+  }
+}
+</style>
--- src/components/PlotView/UniversityCard.vue
+++ src/components/PlotView/UniversityCard.vue
@@ -1,21 +1,48 @@
 <template>
-  <CommonCard title="武汉市高校学生统计"> </CommonCard>
+  <CommonCard title="武汉市高校学生统计">
+    <div class="list">
+      <div v-for="item in data" :key="item.title" class="item">
+        <p>{{ item.title + item.value + item.quantifier }}</p>
+        <img :src="item.src" />
+      </div>
+    </div>
+  </CommonCard>
 </template>
 
 <script setup>
 import CommonCard from './CommonCard.vue'
-import {} from '@opd/g2plot-vue'
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 
-const config = ref({})
-
-const renderChart = data => {
-  config.value = {
-    data,
-  }
-}
-
-onMounted(async () => {})
+const data = ref([
+  {
+    title: '高校',
+    value: '130',
+    quantifier: '家',
+    src: './icons/学校.png',
+  },
+  {
+    title: '在校大学生',
+    value: '100',
+    quantifier: '万',
+    src: './icons/学生.png',
+  },
+])
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.list {
+  display: flex;
+  padding-top: 1.3rem;
+  font-size: 0.8rem;
+  .item {
+    flex: 1;
+    display: grid;
+    justify-items: center;
+    align-content: center;
+    gap: 1rem;
+    img {
+      display: block;
+    }
+  }
+}
+</style>
